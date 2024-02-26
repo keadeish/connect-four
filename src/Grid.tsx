e8ca40ef4557dbae5daec685ac7c2f5ae265740c
Keadeish <you@example.com>
2024-02-26 21:10:24 +0000

    connect 4

---
+++ src/Grid.tsx
@@ -0,0 +1,30 @@
+export function Grid():JSX.Element {
+    return <div>HI
+
+    <table id="table">
+        <tr>
+            <th>1</th>
+            <th>1</th>
+            <th>1</th>
+            <th>1</th>
+            <th>1</th>
+            <th>1</th>
+            <th>1</th>
+            <th>1</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+        </tr>
+    </table>
+
+
+
+    </div>
+}
